3cdbd3cd308e374f9ccf46627f687228e7895667
rowmatrix <romayibar@gmail.com>
2016-10-08 15:25:45 -0700

    Updated README and comment in app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def received_message(event):
 
 def send_text_message(recipient_id, message_text):
 
-    # should probably include encoding/decoding utf-8 function somewhere
+    # encode('utf-8') included to log emojis to heroku logs
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text.encode('utf-8')))
 
     message_data = json.dumps({
