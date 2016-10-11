f9310d834a9ea2b876eecbe6bee12e8297307e3f
rowmatrix <romayibar@gmail.com>
2016-10-11 16:56:26 -0700

    Adds rmwelcome.sh

---
+++ rmwelcome.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -X DELETE -H "Content-Type: application/json" -d '{
+  "setting_type":"call_to_actions",
+  "thread_state":"new_thread"
+}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=$PAGE_ACCESS_TOKEN"
